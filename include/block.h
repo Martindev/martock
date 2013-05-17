191c819c130b4641af7e2a4e2c33c9b5c5cf0e06
Payton Turnage <paytonturnage@gmail.com>
2013-05-16 21:24:54 -0400

    Add shading to block renderer.

--- include/block.h
+++ include/block.h
@@ -5,31 +5,55 @@
 
 /* Block id's. */
 enum {
-        BLOCK_SKY   = 0,
+        BLOCK_SKY = 0,
         BLOCK_GRASS = 1,
-        BLOCK_SOIL  = 2,
+        BLOCK_SOIL = 2,
         BLOCK_STONE = 3,
-        BLOCK_SAND  = 4,
+        BLOCK_SAND = 4,
         BLOCK_WATER = 5
 };
 
 /* Block metadata. */
 enum {
-        BLOCK_COUNT = 6,  /* Quantity of blocks. */
-        BLOCK_SIZE  = 60, /* Sprite resolution. */
-        BLOCK_SCALE = 3,  /* Render scale. */
+        BLOCK_COUNT = 6, /* Quantity of blocks. */
+        BLOCK_SIZE = 60, /* Sprite resolution. */
+        BLOCK_SCALE = 3, /* Render scale. */
+};
+
+/* Visibilities. */
+enum {
+        BLOCK_TRANSPARENT = 0,
+        BLOCK_OPAQUE = 255
 };
 
 typedef struct block {
         u16 id;
-        void *sprite;
 } block;
+
+typedef struct block_profile {
+        u8 opacity; /* Render opacity. */
+        u8 density; /* Difficulty to break. */
+        u8 gravity; /* Does it fall? */
+        u8 weight; /* Fall speed/pack weight. */
+} block_profile;
 
 /* Load the blocks texture file. */
 int block_init (const char *tex);
 
+/* Render a tile. */
+void block_draw (block fg, block bg, int x, int y, int scale);
+
 /* Get a pointer to a sprite's sub bitmap. */
 ALLEGRO_BITMAP *block_sprite(u16 id);
+
+/* Get a pointer to a sprite's background sub bitmap. */
+ALLEGRO_BITMAP *block_back (u16 id);
+
+/* Profile Accessers */
+u8 block_opacity (u16 id);
+u8 block_density (u16 id);
+u8 block_gravity (u16 id);
+u8 block_weight (u16 id);
 
 /* Free the block module's memory. */
 void blocks_deinit ();
