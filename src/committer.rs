15aa73620ca66eaebfe9fbf8d83bb16169d69422
Payton Turnage <paytonturnage@gmail.com>
2016-12-27 22:37:49 -0500

    Stub Comitter trait.

--- src/committer.rs
+++ src/committer.rs
@@ -1,3 +1,11 @@
+use body;
 use commit;
+use world;
 
-pub trait CL: Iterator<Item=commit::Commit> {}
+pub trait CL {
+    fn commits(&self) -> Iterator<Item=commit::Commit>;
+}
+
+pub trait Committer {
+    fn cl(&world::World, &[&body::Body]) -> Option<Box<CL>>;
+}
--- src/main.rs
+++ src/main.rs
@@ -2,6 +2,7 @@ mod world;
 mod commit;
 mod committer;
 mod block;
+mod body;
 mod tool;
 
 fn main() {}
