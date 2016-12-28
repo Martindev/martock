20f6a5aded409cd757c2ee7553b5a3feb36187b3
Payton Turnage <paytonturnage@gmail.com>
2016-12-28 00:11:51 -0500

    Module document world & body.

--- src/body.rs
+++ src/body.rs
@@ -1,1 +1,3 @@
+/// Body is the interface by which the world engine interacts with anything which has a
+/// representation in the world which is subject to physics.
 pub trait Body {}
--- src/world.rs
+++ src/world.rs
@@ -1,6 +1,13 @@
+//! world is the objective world manager for martock.
+
+/// World manages changing and accessing the state of the world, which is a three dimensional array
+/// of blocks which can vary in state and behavior. World is explicitly not responsible for
+/// determining the content of the world by itself; World should not modify the state of the world
+/// except in fulfillment of a user's request.
 pub struct World;
 
 impl World {
+    /// new creates a new world
     pub fn new() -> Self {
         World{}
     }
