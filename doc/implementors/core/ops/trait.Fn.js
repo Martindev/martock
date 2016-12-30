bff9f8e3fbcac397b3fbb8bc5545585a2e8b0844
Martbot <Martbot@martbot>
2016-12-30 09:36:31 +0000

    martock doc hook

--- doc/implementors/core/ops/trait.Fn.js
+++ doc/implementors/core/ops/trait.Fn.js
@@ -1,5 +1,5 @@
 (function() {var implementors = {};
-implementors["libc"] = [];implementors["arrayvec"] = [];implementors["lazy_static"] = [];implementors["num"] = [];implementors["sdl2"] = [];implementors["gl"] = [];implementors["enum_primitive"] = [];implementors["graphics"] = [];implementors["sdl2_window"] = [];implementors["image"] = [];implementors["opengl_graphics"] = [];
+implementors["gl"] = [];implementors["libc"] = [];implementors["lazy_static"] = [];implementors["graphics"] = [];implementors["arrayvec"] = [];implementors["num"] = [];implementors["enum_primitive"] = [];implementors["sdl2"] = [];implementors["sdl2_window"] = [];implementors["image"] = [];implementors["opengl_graphics"] = [];
 
             if (window.register_implementors) {
                 window.register_implementors(implementors);
--- doc/implementors/rand/reseeding/trait.Reseeder.js
+++ doc/implementors/rand/reseeding/trait.Reseeder.js
@@ -1,5 +1,5 @@
 (function() {var implementors = {};
-implementors["libc"] = [];implementors["rand"] = [];implementors["arrayvec"] = [];implementors["lazy_static"] = [];implementors["num"] = [];implementors["sdl2"] = [];implementors["sdl2"] = [];implementors["gl"] = [];implementors["enum_primitive"] = [];implementors["graphics"] = [];implementors["sdl2_window"] = [];implementors["sdl2_window"] = [];implementors["image"] = [];implementors["image"] = [];implementors["opengl_graphics"] = [];implementors["opengl_graphics"] = [];
+implementors["gl"] = [];implementors["libc"] = [];implementors["lazy_static"] = [];implementors["graphics"] = [];implementors["arrayvec"] = [];implementors["num"] = [];implementors["enum_primitive"] = [];implementors["rand"] = [];implementors["sdl2"] = [];implementors["sdl2"] = [];implementors["sdl2_window"] = [];implementors["sdl2_window"] = [];implementors["image"] = [];implementors["image"] = [];implementors["opengl_graphics"] = [];implementors["opengl_graphics"] = [];
 
             if (window.register_implementors) {
                 window.register_implementors(implementors);
