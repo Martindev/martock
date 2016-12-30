faa39c46e01ea00c18caaa3ebde8d37199703255
Martbot <Martbot@martbot>
2016-12-30 20:12:25 +0000

    martock doc hook

--- doc/implementors/rand/reseeding/trait.Reseeder.js
+++ doc/implementors/rand/reseeding/trait.Reseeder.js
@@ -1,5 +1,5 @@
 (function() {var implementors = {};
-implementors["lazy_static"] = [];implementors["arrayvec"] = [];implementors["num"] = [];implementors["enum_primitive"] = [];implementors["libc"] = [];implementors["gl"] = [];implementors["rand"] = [];implementors["sdl2"] = [];implementors["sdl2"] = [];implementors["graphics"] = [];implementors["sdl2_window"] = [];implementors["sdl2_window"] = [];implementors["image"] = [];implementors["image"] = [];implementors["opengl_graphics"] = [];implementors["opengl_graphics"] = [];
+implementors["libc"] = [];implementors["rand"] = [];implementors["num"] = [];implementors["enum_primitive"] = [];implementors["graphics"] = [];implementors["gl"] = [];implementors["arrayvec"] = [];implementors["lazy_static"] = [];implementors["sdl2"] = [];implementors["sdl2"] = [];implementors["sdl2_window"] = [];implementors["sdl2_window"] = [];implementors["image"] = [];implementors["image"] = [];implementors["opengl_graphics"] = [];implementors["opengl_graphics"] = [];
 
             if (window.register_implementors) {
                 window.register_implementors(implementors);
