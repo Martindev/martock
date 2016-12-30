079a6d2c9f2840b33b1c8d63866dc28cae52b29c
jacob-zimmerman <megashadowzx@gmail.com>
2016-12-30 15:37:57 -0500

    Stubbed out procedural generator.

---
+++ src/procedural_generator.rs
@@ -0,0 +1,37 @@
+//! ProceduralGenerator provides a CL for iteration when new terrain needs to be generated.
+//!
+//! There can only be one ProceduralGenerator modifying the world.
+//!
+//! ProceduralGenerator is responsible for generating chunks of blocks as needed.
+//!
+//! It is also the job of the ProceduralGenerator to report whether or not a block has been 
+//! generated for a given location.
+
+use block;
+use committer;
+use world;
+
+let chunk_width, world_height = 0;
+
+pub type Chunk = [ [Block, ..chunk_width], ..world_height]
+
+pub struct ProceduralGenerator {
+	next_chunk: Chunk
+
+}
+
+impl ProceduralGenerator {
+	fn
+}
+
+impl committer:Committer for ProceduralGenerator {
+	fn cl(&self, w: &world::World) -> Option<Box<CL>> {
+		None
+	}
+}
+
+impl CL for ProceduralGenerator {
+	fn next(&mut self) -> Option<Self::Item> {
+		None
+	}
+}
