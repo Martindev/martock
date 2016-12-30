0531c8a3b5f3bfcfdb73c47d4b716c32c91d1f26
Martbot <Martbot@martbot>
2016-12-30 03:29:49 +0000

    martock doc hook

--- doc/implementors/std/sys/imp/ext/process/trait.ExitStatusExt.js
+++ doc/implementors/std/sys/imp/ext/process/trait.ExitStatusExt.js
@@ -1,5 +1,5 @@
 (function() {var implementors = {};
-implementors["libc"] = [];implementors["num"] = [];implementors["enum_primitive"] = [];implementors["gl"] = [];implementors["lazy_static"] = [];implementors["arrayvec"] = [];implementors["sdl2"] = [];implementors["graphics"] = [];implementors["sdl2_window"] = [];implementors["image"] = [];implementors["opengl_graphics"] = [];
+implementors["lazy_static"] = [];implementors["libc"] = [];implementors["gl"] = [];implementors["arrayvec"] = [];implementors["graphics"] = [];implementors["num"] = [];implementors["enum_primitive"] = [];implementors["sdl2"] = [];implementors["sdl2_window"] = [];implementors["image"] = [];implementors["opengl_graphics"] = [];
 
             if (window.register_implementors) {
                 window.register_implementors(implementors);
